e0da91ca28eb291f2e5e3710de02b9a1ea8f5fd8
mxw8277 <mark_williams@honedepot.com>
2016-10-03 14:03:35 -0400

    adding project files

---
+++ www/js/app.js
@@ -0,0 +1,15 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+angular.module('RealDeal', ['ionic'])
+
+.controller('PropertyCtrl', function($scope) {
+  $scope.tasks = [
+    { title: 'Collect coins' },
+    { title: 'Eat mushrooms' },
+    { title: 'Get high enough to grab the flag' },
+    { title: 'Find the Princess' }
+  ];
+})
